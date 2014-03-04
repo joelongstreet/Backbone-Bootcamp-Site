18cc6b209a9c8068abc948fbedfb655f9486f2bf
Joe Longstreet <joelongstreet@gmail.com>
2014-03-03 23:05:50 -0600

    only call methods when necessary

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
+var $skeletor = null;
+var $skeletorParts = null;
+var watchers = {};
+
+
 $(function(){
+  // Set odd elements as sticky
   $('#section-3').sticky();
   $('#section-5').sticky();
   $('#section-7').sticky();
 
-  var $skeletor = $('.skeletor');
-  var $skeletorParts = $skeletor.find('img');
 
+  // Setup skeletor
+  $skeletor = $('.skeletor');
+  $skeletorParts = $skeletor.find('img');
+
+
+  // Set up scrolling and make active when
+  // items enter viewport
+  var $sections = $('section');
   $(window).scroll(function(){
-    var scrollPosition = $(window).scrollTop();
-
-    // Set the bone parts
-    $.each($skeletorParts, function(index, item){
-      var reverseIndex = $skeletorParts.length - index;
-      var itemVal = -1*(reverseIndex * scrollPosition/10);
-      $(item).css('transform', 'translateY(' + itemVal + 'px)');
+    $sections.each(function(index, item){
+      if(inViewport(item)){
+        var methodName = 'section' + index;
+        if(watchers[methodName])
+          watchers[methodName]();
+      }
     });
-
-    // Move ther wrapper around for better visuals
-    var wrapperVal = scrollPosition/10;
-    $skeletor.css('transform', 'translateY(' + wrapperVal + 'px)');
   });
 });
+
+
+
+watchers.section1 = function(){
+  var scrollPosition = $(window).scrollTop();
+
+  // Set the bone parts
+  $.each($skeletorParts, function(index, item){
+    var reverseIndex = $skeletorParts.length - index;
+    var itemVal = -1*(reverseIndex * scrollPosition/10);
+    $(item).css('transform', 'translateY(' + itemVal + 'px)');
+  });
+
+  // Move ther wrapper around for better visuals
+  var wrapperVal = scrollPosition/10;
+  $skeletor.css('transform', 'translateY(' + wrapperVal + 'px)');
+};
+
+
+
+var inViewport = function(el){
+  var rect = el.getBoundingClientRect();
+
+  if(rect.bottom >= 0 && rect.bottom < window.innerHeight)
+    return true;
+  else if(rect.top >=0 && rect.top < window.innerHeight)
+    return true;
+  else return false;
+};
